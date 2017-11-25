f3636607e3a24c8120f807b508ff7cd40a6a7ae3
Man Tsui <33650575+mantsui@users.noreply.github.com>
2017-11-24 18:45:09 -0800

    Update Tableau_JS_Embed.js

--- Tableau_JS_Embed.js
+++ Tableau_JS_Embed.js
@@ -101,7 +101,7 @@ window.onload= function() {
 	vizOPMedicareAvgPayment = new tableau.Viz(placeholder04, vizURL04, options04);
 
 	var placeholder05 = document.getElementById('myOPMedicareAvgPayment');
-	var vizURL05 = 'https://public.tableau.com/views/MedicareChargeProject_0/DistributionAnalysisOutpatientMedicareAvgPayment';
+	var vizURL05 = 'https://public.tableau.com/views/MedicareChargeProject_0/IPOPPaymentDashboard';
 	var options05 = {
 		width: '1280px',
 		height: '720px',
